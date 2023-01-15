544291e9849b361e3bc3cf8ee804c704bfb72db4
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-15 21:02:10 +0500

    added middleware that validation

---
+++ Football/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation.TestHelper;
+using Football.Domain.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace Football.Api.Middlewares;
+public class GlobalExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(ValidationExceptions validationException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            await HandleException(context, validationException.Message);
+        }
+        catch(NotFoundExcaptions notfoundException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+
+            var serilizedOblect = JsonSerializer.Serialize(new
+            {
+                notfoundException.Message,
+            });
+
+            await HandleException(context, serilizedOblect);
+        }
+        catch (Exception exception)
+        {
+            await HandleException(context, exception.Message);
+        }
+    }
+    private async Task HandleException(HttpContext context, string message)
+    {
+        context.Response.ContentType= "application/json";
+
+        await context.Response.WriteAsync(message);
+    }
+}
