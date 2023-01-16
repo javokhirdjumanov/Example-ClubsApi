d0de674f3c41d24533546dc9314024ac2a4c6162
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-16 20:40:43 +0500

    added authentication to services

---
+++ Football.Application/Services/Authentications/Authentications.cs
@@ -0,0 +1,40 @@
+﻿using Football.Application.DataTransferObjects.Authentication;
+using Football.Infrastructure.Authentication;
+using Football.Infrastructure.Repositories.UserRepositories;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Football.Application.Services.Authentications;
+public class Authentications : IAuthentication
+{
+    /// <summary>
+    /// C O N S T R U C T O R
+    /// </summary>
+    private readonly IUserRepositiory userRepositiory;
+    private readonly IJwtTokenHandler jwtTokenHandler;
+    public Authentications(IUserRepositiory userRepositiory, IJwtTokenHandler jwtTokenHandler)
+    {
+        this.userRepositiory = userRepositiory;
+        this.jwtTokenHandler = jwtTokenHandler;
+    }
+
+    /// <summary>
+    /// L O G I N
+    /// </summary>
+    public async Task<TokenDto> LoginAsync(AuthenticationsDto authenticationsDto)
+    {
+        var storegeUser = await this.userRepositiory.SelectByIdWithDetaialsAsync(
+            expression: user => 
+            user.Email == authenticationsDto.email && user.PasswordHash == authenticationsDto.password,
+
+            includes: Array.Empty<string>()
+            );
+
+        var token = this.jwtTokenHandler.GenerationJwtToken(storegeUser);
+
+        return new TokenDto(
+            accessToken: new JwtSecurityTokenHandler().WriteToken(token),
+            refleshToken: null,
+            expireDate: token.ValidTo
+            );
+    }
+}
---
+++ Football.Application/Services/Authentications/IAuthentication.cs
@@ -0,0 +1,7 @@
+﻿using Football.Application.DataTransferObjects.Authentication;
+
+namespace Football.Application.Services.Authentications;
+public interface IAuthentication
+{
+    Task<TokenDto> LoginAsync(AuthenticationsDto authenticationsDto);
+}
